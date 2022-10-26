1c3696c78bc1026cec195f5eb3f8e998acf31e9f
GermanMT <germanoctako@gmail.com>
2022-10-26 12:45:36 +0200

    chore: Add flamapy-dn as required dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setuptools.setup(
     python_requires = '>=3.10',
     install_requires = [
         'z3-solver==4.11.2.0',
-        'flamapy==1.0.1'
+        'flamapy==1.0.1',
+        'flamapy-dn==1.0.1'
     ],
     tests_requires = [
         'prospector[with_everything]==1.7.7',
