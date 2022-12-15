cb3d8b301edc2532cfe0aafa3f0a191cf39f9de2
GermanMT <germanoctako@gmail.com>
2022-12-15 21:22:55 +0100

    feat: Upgrade dn plugin

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     install_requires=[
         'z3-solver==4.11.2.0',
         'flamapy==1.0.1',
-        'flamapy-dn==1.1.3'
+        'flamapy-dn==1.1.4'
     ],
     tests_requires=[
         'prospector[with_everything]==1.8.3',
