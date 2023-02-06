8f8d57afd38d8cf6dec00b3b0f60379ee7681086
GermanMT <germanoctako@gmail.com>
2023-02-06 15:30:10 +0100

    chore: Update imports

--- flamapy/metamodels/smt_metamodel/operations/__init__.py
+++ flamapy/metamodels/smt_metamodel/operations/__init__.py
@@ -3,5 +3,17 @@ from .number_of_products import NumberOfProducts
 from .filter_configs import FilterConfigs
 from .maximize_impact import MaximizeImpact
 from .minimize_impact import MinimizeImpact
+from .valid_config import ValidConfig
+from .complete_config import CompleteConfig
+from .config_by_impact import ConfigByImpact
 
-__all__ = ['ValidModel', 'NumberOfProducts', 'FilterConfigs', 'MaximizeImpact', 'MinimizeImpact']
+__all__ = [
+    'ValidModel',
+    'NumberOfProducts',
+    'FilterConfigs',
+    'MaximizeImpact',
+    'MinimizeImpact',
+    'ValidConfig',
+    'CompleteConfig',
+    'ConfigByImpact'
+]
