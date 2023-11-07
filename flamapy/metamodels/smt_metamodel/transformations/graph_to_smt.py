403efe7cf4d723f4d639a50ac81316f7e460928d
GermanMT <germanoctako@gmail.com>
2023-11-07 14:03:00 +0100

    fix: Refactor SMT constraints to make it much more efficient

--- flamapy/metamodels/smt_metamodel/transformations/graph_to_smt.py
+++ flamapy/metamodels/smt_metamodel/transformations/graph_to_smt.py
@@ -1,11 +1,15 @@
 from typing import Any
 
-from operator import eq, lt, gt, le, ge
+from z3 import And, ArithRef, BoolRef, Implies, Int, Or, Real, Not, If
 
-from z3 import And, ArithRef, BoolRef, Implies, Int, Or, Real, Not
-
-from univers.versions import Version
-from univers.version_range import VersionRange
+from univers.versions import Version, PypiVersion, SemverVersion, MavenVersion, InvalidVersion
+from univers.version_range import (
+    VersionRange,
+    PypiVersionRange,
+    NpmVersionRange,
+    MavenVersionRange,
+    InvalidVersionRange
+)
 
 from flamapy.core.transformations import Transformation
 from flamapy.metamodels.smt_metamodel.models.pysmt_model import PySMTModel
@@ -26,120 +30,95 @@ class GraphToSMT(Transformation):
         source_data: dict[str, Any],
         file_name: str,
         package_manager: str,
-        agregator: str | None = None
+        agregator: str
     ) -> None:
-        self.source_data: dict[str, Any] = source_data
+        self.source_data: dict[str, list[dict[str, Any]]] = source_data
         self.file_name: str = file_name
-        self.package_manager: str = package_manager
-        self.agregator: str | None = agregator
+        self.agregator: str = agregator
         self.destination_model: PySMTModel = PySMTModel()
-        self.dependency_versions: dict[str, Any] = self.match_dependency_versions()
-        self.version_type, self.range_type = self.get_version_range_type()
+        self.dependency_versions: dict[str, list[dict[str, Any]]] = (
+            self.match_dependency_versions()
+        )
+        self.version_type, self.range_type = self.get_version_range_type(package_manager)
         self.vars: dict[str, ArithRef] = {}
-        self.childs: dict[BoolRef, list[BoolRef]] = {}
-        self.parents: dict[ArithRef, list[BoolRef]] = {}
+        self.childs: dict[ArithRef, dict[ArithRef, list[int]]] = {}
+        self.parents: dict[ArithRef, dict[ArithRef, list[int]]] = {}
         self.directs: list[str] = []
-        self.cvss_p: list[ArithRef] = []
         self.domain: list[BoolRef] = []
-        self.distributions: list[str] = []
-        self.ctcs: list[BoolRef] = []
-        self.operations = {
-            '==': eq,
-            '<': lt,
-            '>': gt,
-            '<=': le,
-            '>=': ge
-        }
+        # self.distributions: list[str] = []
+        self.ctcs: dict[ArithRef, dict[float, list[int]]] = {}
 
     def match_dependency_versions(self) -> dict[str, Any]:
-        dependency_versions: dict[str, Any] = {}
+        dependency_versions: dict[str, list[dict[str, Any]]] = {}
         for rel_have in self.source_data['have']:
-            if rel_have['dependency'] in dependency_versions:
-                dependency = rel_have.pop('dependency')
-                dependency_versions[dependency].append(rel_have)
-            else:
-                dependency = rel_have.pop('dependency')
-                dependency_versions[dependency] = [rel_have]
+            dependency = rel_have.pop('dependency')
+            dependency_versions.setdefault(dependency, []).append(rel_have)
         return dependency_versions
 
     def transform(self) -> None:
+        impacts: list[ArithRef] = []
         for rel_requires in self.source_data['requires']:
             if rel_requires['parent_type'] == 'RequirementFile':
-                self.transform_direct_package(rel_requires)
+                impacts.extend(self.transform_direct_package(rel_requires))
             else:
-                self.transform_indirect_package(rel_requires)
+                impacts.extend(self.transform_indirect_package(rel_requires))
         self.build_indirect_constraints()
-        self.domain.extend(self.ctcs)
-
-        cvss_f_var = Real('CVSS' + self.file_name)
-        agregator_impact = self.agregate(self.cvss_p) if self.cvss_p else 0.
-        self.domain.append(cvss_f_var == agregator_impact)
-
+        self.build_impact_constraints()
+        self.domain.append(Real('CVSS' + self.file_name) == self.agregate(impacts))
         func_obj_var = Real('func_obj_' + self.file_name)
-        func_obj_impact = self.obj_func(self.cvss_p) if self.cvss_p else 0.
-        self.domain.append(func_obj_var == func_obj_impact)
-
+        self.domain.append(func_obj_var == self.mean(impacts))
         self.destination_model.domain = And(self.domain)
         self.destination_model.func_obj_var = func_obj_var
 
-    def transform_direct_package(self, rel_requires: dict[str, Any]) -> None:
+    def transform_direct_package(self, rel_requires: dict[str, Any]) -> list[ArithRef]:
+        impacts: list[ArithRef] = []
         self.directs.append(rel_requires['dependency'])
         if rel_requires['dependency'] not in self.vars:
             var = Int(rel_requires['dependency'])
             self.vars[rel_requires['dependency']] = var
-
             cvss_p_name = 'CVSS' + rel_requires['dependency']
             cvss_p_var = Real(cvss_p_name)
             self.vars[cvss_p_name] = cvss_p_var
-            self.cvss_p.append(cvss_p_var)
+            impacts.append(cvss_p_var)
         else:
             var = self.vars[rel_requires['dependency']]
             cvss_p_var = self.vars['CVSS' + rel_requires['dependency']]
-
         filtered_versions = self.filter_versions(
             rel_requires['dependency'],
             rel_requires['constraints']
         )
-
         self.build_direct_contraint(var, filtered_versions)
-
         self.transform_versions(filtered_versions, var, cvss_p_var)
+        return impacts
 
     def transform_indirect_package(
         self,
         rel_requires: dict[str, Any]
-    ) -> None:
+    ) -> list[ArithRef]:
+        impacts: list[ArithRef] = []
         if rel_requires['dependency'] not in self.vars:
             var = Int(rel_requires['dependency'])
             self.vars[rel_requires['dependency']] = var
-
             cvss_p_name = 'CVSS' + rel_requires['dependency']
             cvss_p_var = Real(cvss_p_name)
             self.vars[cvss_p_name] = cvss_p_var
-            self.cvss_p.append(cvss_p_var)
-
-            ctc = Implies(var == -1, cvss_p_var == 0.)
-            self.ctcs.append(ctc)
+            impacts.append(cvss_p_var)
+            self.ctcs.setdefault((var, cvss_p_var), {}).setdefault(0., []).append(-1)
         else:
             var = self.vars[rel_requires['dependency']]
             cvss_p_var = self.vars['CVSS' + rel_requires['dependency']]
-
-        parent_name = self.get_parent_name(rel_requires['parent_id'])
-        parent = self.vars[parent_name]
-
         filtered_versions = self.filter_versions(
             rel_requires['dependency'],
             rel_requires['constraints']
         )
-
         self.append_indirect_constraint(
             var,
             filtered_versions,
-            parent,
+            self.vars[self.get_parent_name(rel_requires['parent_id'])],
             rel_requires['parent_count']
         )
-
         self.transform_versions(filtered_versions, var, cvss_p_var)
+        return impacts
 
     def transform_versions(
         self,
@@ -148,18 +127,13 @@ class GraphToSMT(Transformation):
         cvss_p_var: ArithRef
     ) -> None:
         for version in versions:
-            key = str(var) + '-' + str(version['count'])
-            if key not in self.distributions:
-                match self.agregator:
-                    case 'mean':
-                        v_impact = version['mean']
-                    case 'weighted_mean':
-                        v_impact = version['weighted_mean']
-                    case _:
-                        v_impact = version['mean']
-                ctc = Implies(var == version['count'], cvss_p_var == v_impact)
-                self.ctcs.append(ctc)
-                self.distributions.append(key)
+            # key = str(var) + '-' + str(version['count'])
+            # if key not in self.distributions:
+            self.ctcs.setdefault((var, cvss_p_var), {}).setdefault(
+                version[self.agregator],
+                []
+            ).append(version['count'])
+            # self.distributions.append(key)
 
     def get_parent_name(self, version_id: str) -> str:
         for dependency, versions in self.dependency_versions.items():
@@ -172,71 +146,67 @@ class GraphToSMT(Transformation):
         filtered_versions = []
         if constraints != 'any':
             for version in self.dependency_versions[dependency]:
+                try:
+                    univers_version = self.version_type(version['release'])
+                except InvalidVersion:
+                    continue
                 check = True
-                if self.package_manager == 'PIP':
-                    for constraint in constraints.split(','):
-                        versions_range = self.range_type.from_native(constraint)
-                        check = check and self.version_type(version['release']) in versions_range
-                else:
+                try:
                     versions_range = self.range_type.from_native(constraints)
-                    check = check and self.version_type(version['release']) in versions_range
+                    print(univers_version)
+                    print(versions_range)
+                    check = check and univers_version in versions_range
+                except InvalidVersionRange:
+                    continue
                 if check:
                     filtered_versions.append(version)
             return filtered_versions
         return self.dependency_versions[dependency]
 
-    def build_direct_contraint(self, var: ArithRef, versions: list[dict[str, Any]]) -> None:
-        constraint = [var == version['count'] for version in versions]
-        if constraint:
-            self.domain.append(Or(constraint))
-
     def append_indirect_constraint(
         self,
-        var: ArithRef,
+        child: ArithRef,
         versions: list[dict[str, Any]],
         parent: ArithRef,
         version: int
     ) -> None:
-        parts = [var == version['count'] for version in versions]
-        if parts:
-            versions = Or(parts)
-            if versions in self.childs:
-                self.childs[versions].append(parent == version)
-            else:
-                self.childs[versions] = [parent == version]
+        if versions:
+            version_constraints = self.group_versions(
+                child,
+                [version['count'] for version in versions]
+            )
+            self.childs.setdefault(version_constraints, {}).setdefault(parent, []).append(version)
+            if str(child) not in self.directs:
+                self.parents.setdefault(child, {}).setdefault(parent, []).append(version)
 
-        if str(var) not in self.directs:
-            if var in self.parents:
-                self.parents[var].append(parent == version)
-            else:
-                self.parents[var] = [parent == version]
+    def build_direct_contraint(self, var: ArithRef, versions: list[dict[str, Any]]) -> None:
+        if versions:
+            version_constraints = self.group_versions(
+                var,
+                [version['count'] for version in versions]
+            )
+            self.domain.append(version_constraints)
+        else:
+            self.domain.append(False)
 
     def build_indirect_constraints(self) -> None:
-        for versions, parents in self.childs.items():
-            self.domain.append(Implies(Or(parents), versions))
-        for var, parents in self.parents.items():
-            self.domain.append(Implies(Not(Or(parents)), var == -1))
+        for versions, _ in self.childs.items():
+            for parent, parent_versions in _.items():
+                self.domain.append(Implies(self.group_versions(parent, parent_versions), versions))
+        for child, _ in self.parents.items():
+            for parent, parent_versions in _.items():
+                self.domain.append(
+                    Implies(Not(self.group_versions(parent, parent_versions)), child == -1)
+                )
 
-    def get_version_range_type(self) -> tuple[Version, VersionRange]:
-        match self.package_manager:
-            case 'PIP':
-                from univers.versions import PypiVersion
-                from univers.version_range import PypiVersionRange
-                return PypiVersion, PypiVersionRange
-            case 'NPM':
-                from univers.versions import SemverVersion
-                from univers.version_range import NpmVersionRange
-                return SemverVersion, NpmVersionRange
-            case 'MVN':
-                from univers.versions import MavenVersion
-                from univers.version_range import MavenVersionRange
-                return MavenVersion, MavenVersionRange
-            case _:
-                from univers.versions import PypiVersion
-                from univers.version_range import PypiVersionRange
-                return PypiVersion, PypiVersionRange
+    def build_impact_constraints(self) -> None:
+        for vars_, _ in self.ctcs.items():
+            for impact, versions in _.items():
+                self.domain.append(
+                    Implies(self.group_versions(vars_[0], versions), vars_[1] == impact)
+                )
 
-    # TODO: Posibilidad de añadir nuevas métricas
+    # TODO: Possibility to add new metrics
     def agregate(
         self,
         impacts: list[ArithRef],
@@ -246,28 +216,47 @@ class GraphToSMT(Transformation):
                 return self.mean(impacts)
             case 'weighted_mean':
                 return self.weighted_mean(impacts)
-            case _:
-                return self.mean(impacts)
 
     @staticmethod
-    def obj_func(problems: list[ArithRef]) -> ArithRef:
-        return sum(problems)
+    def get_version_range_type(package_manager: str) -> tuple[Version, VersionRange]:
+        match package_manager:
+            case 'PIP':
+                return PypiVersion, PypiVersionRange
+            case 'NPM':
+                return SemverVersion, NpmVersionRange
+            case 'MVN':
+                return MavenVersion, MavenVersionRange
+        return PypiVersion, PypiVersionRange
+
+    @staticmethod
+    def group_versions(var: ArithRef, versions: list[int]) -> BoolRef:
+        min_ = min(versions)
+        max_ = max(versions)
+        constraints: list[BoolRef] = []
+        while min_ <= max_:
+            temporal_list = []
+            while min_ not in versions:
+                min_ += 1
+            while min_ in versions:
+                temporal_list.append(min_)
+                min_ += 1
+            if temporal_list:
+                if len(temporal_list) == 1:
+                    constraints.append(var == temporal_list[0])
+                else:
+                    constraints.append(And(var >= temporal_list[0], var <= temporal_list[-1]))
+        return Or(constraints)
 
     @staticmethod
     def mean(impacts: list[ArithRef]) -> ArithRef:
         if impacts:
-            return sum(impacts) / len(impacts)
+            num_non_zero = sum([If(val == 0, 0, 1) for val in impacts])
+            return If(num_non_zero == 0., 0., sum(impacts) / num_non_zero)
         return 0.
 
     @staticmethod
     def weighted_mean(impacts: list[ArithRef]) -> ArithRef:
         if impacts:
-            dividends = []
-            divisors = []
-
-            for var in impacts:
-                dividends.append(var**2 * 0.1)
-                divisors.append(var * 0.1)
-
-            return sum(dividends) / sum(divisors)
+            divisors = sum([val * 0.1 for val in impacts])
+            return If(divisors == 0., 0, sum([val**2 * 0.1 for val in impacts]) / divisors)
         return 0.
