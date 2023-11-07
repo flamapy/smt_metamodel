f96dbe447e7c198f7ccbf8362b8983d44ddef6dd
GermanMT <germanoctako@gmail.com>
2023-11-07 14:47:26 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='flamapy-smt',
-    version='1.3',
+    version='1.3.1',
     author='Antonio Germán Márquez Trujillo',
     author_email='amtrujillo@us.es',
     description='This repo host the SMT model concrete classes',
